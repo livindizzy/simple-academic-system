f26972087f6ecd6e7490df0e25fc27b4a23068c5
Abdul Aziz Rizqi N <abdulazizrn14@gmail.com>
2026-03-11 07:36:58 +0700

    init project

---
+++ src/main/java/com/academic/simple_academic_system/SimpleAcademicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.academic.simple_academic_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAcademicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAcademicSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academic/simple_academic_system/SimpleAcademicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic.simple_academic_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAcademicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
